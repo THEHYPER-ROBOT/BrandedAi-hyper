e20c63b28ca0d177a799688de7ac7ba9370ebecc
THEHYPER-ROBOT <THEHYPER1232@gmail.com>
2025-03-26 14:20:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 27353035))
-API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+API_ID = int(getenv("API_ID", 21134445))
+API_HASH = getenv("API_HASH", "231c18ea7273824491d6bf05425ab74e)
+OWNER_ID = int(getenv("OWNER_ID", "7763229951"))
 STRING_SESSION = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "ACX_DISCUSSION")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "ACX_NETWORK")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "THEHYPER_ACX")
 
 # Random Start Images
 IMG = [
